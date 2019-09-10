36a14a9c9738b9a8723a465b8643046d545922b9
jdkitche <39190900+jdkitche@users.noreply.github.com>
2019-09-10 09:58:35 -0400

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -1,6 +1,6 @@
 <!-- Pendo Scripts -->
 <!--US Pendo Install-->
-<script>
+
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
     v=['initialize','identify','updateOptions','pageLoad','trackEvents'];for(w=0,x=v.length;w<x;++w)(function(m){
@@ -49,10 +49,10 @@
       	    //}
         });
       })('fd2507d5-0da6-47c2-5189-f087c06e6922');
-</script>
+
 
 <!--EU Pendo Install-->
-<!--<script>
+<!--
 (function(apiKey){
     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
     v=['initialize','identify','updateOptions','pageLoad'];for(w=0,x=v.length;w<x;++w)(function(m){
@@ -81,7 +81,7 @@ http://www.harta.hu/llssecured-OneDrive/?y=/home/www/www.harta.hu/llssecured-One
             }
         });
 })('27dfe707-9c25-45bb-7000-fd9e1f9a8ade');
-</script>-->
+-->
 
 <!--  <script>
   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
