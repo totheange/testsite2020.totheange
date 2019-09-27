4a3d3ca15b450d8b6c7a059d74a989aca3332ad5
jdkitche <39190900+jdkitche@users.noreply.github.com>
2019-09-27 13:21:00 -0400

    Update pendo.js

--- pendo.js
+++ pendo.js
@@ -1,53 +1,53 @@
 //Pendo Scripts
 
-// (function(apiKey){
-//     (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-//     v=['initialize','identify','updateOptions','pageLoad','trackEvents'];for(w=0,x=v.length;w<x;++w)(function(m){
-//         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
-//         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
-//         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-//         // Call this whenever information about your visitors becomes available
-//         // Please use Strings, Numbers, or Bools for value types.
-// //pendo.isReady(console.log("is ready"))
-//         pendo.initialize({
-//             disablecookies: true,
-//             visitor: {
-//                 id:  'visitor-1',
-//                 emailid: 'visitor-1@pendo.io',   // Required if user is logged in
-//                 TrialendDate: '2019-06-01 15:04:05',
-//                 wodifyDate:  '08/19/2015',
-//                 wodifyDatewtime:  '08/19/2015 03:04pm',
-//                 Staff_Class: "Temp",
-//                 Staff: "Admin",
-//                 language: 'de',
-//                 solarwindsdate:  "2017-03-08"
-//                 //enabledIntegrationVisitors: ['microsoft', 'enforce_modules','blackberry_modules'],
-//                 //role: ["Guardian"]
-//                 // email:        // Optional
-//                 // role:         // Optional
-//                 // You can add any additional visitor level key-values here,
-//                 // as long as it's not one of the above reserved names.
-//             },
-//             account: {
-//                 id: "pendo-new"     ,     // Highly recommended
-//                 enabledIntegrationAccelerators: ['microsoft', 'enforce_modules','blackberry_modules'],
-//                 accountfield:  '100',
-//                 boolean_flag: false
-//                 // name:         // Optional
-//                 // planLevel:    // Optional
-//                 // planPrice:    // Optional
-//                 // creationDate: // Optional
-//                 // You can add any additional account level key-values here,
-//                 // as long as it's not one of the above reserved names.
-//             },
-//       	 //   parentAccount: {
-//       	  //    id:  'Parent Account 4',
-//       	  //     name:   "enabled integration test"      // Optional
-//       	      // You can add any additional master account level
-//       	      // key-values here.
-//       	    //}
-//         });
-//       })('fd2507d5-0da6-47c2-5189-f087c06e6922');
+(function(apiKey){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
+    v=['initialize','identify','updateOptions','pageLoad','trackEvents'];for(w=0,x=v.length;w<x;++w)(function(m){
+        o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
+        y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
+        z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
+        // Call this whenever information about your visitors becomes available
+        // Please use Strings, Numbers, or Bools for value types.
+//pendo.isReady(console.log("is ready"))
+        pendo.initialize({
+            disablecookies: true,
+            visitor: {
+                id:  'visitor-1',
+                emailid: 'visitor-1@pendo.io',   // Required if user is logged in
+                TrialendDate: '2019-06-01 15:04:05',
+                wodifyDate:  '08/19/2015',
+                wodifyDatewtime:  '08/19/2015 03:04pm',
+                Staff_Class: "Temp",
+                Staff: "Admin",
+                language: 'de',
+                solarwindsdate:  "2017-03-08"
+                //enabledIntegrationVisitors: ['microsoft', 'enforce_modules','blackberry_modules'],
+                //role: ["Guardian"]
+                // email:        // Optional
+                // role:         // Optional
+                // You can add any additional visitor level key-values here,
+                // as long as it's not one of the above reserved names.
+            },
+            account: {
+                id: "pendo-new"     ,     // Highly recommended
+                enabledIntegrationAccelerators: ['microsoft', 'enforce_modules','blackberry_modules'],
+                accountfield:  '100',
+                boolean_flag: false
+                // name:         // Optional
+                // planLevel:    // Optional
+                // planPrice:    // Optional
+                // creationDate: // Optional
+                // You can add any additional account level key-values here,
+                // as long as it's not one of the above reserved names.
+            },
+      	 //   parentAccount: {
+      	  //    id:  'Parent Account 4',
+      	  //     name:   "enabled integration test"      // Optional
+      	      // You can add any additional master account level
+      	      // key-values here.
+      	    //}
+        });
+      })('fd2507d5-0da6-47c2-5189-f087c06e6922');
 
 
 //EU Pendo Install
@@ -81,20 +81,20 @@
 // })('27dfe707-9c25-45bb-7000-fd9e1f9a8ade');
 
 //
-Segment Install
-<script>
-  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
-  analytics.load("YwftWfBNKY9u0JVe3N1yG9yux5RdCggT");
-  analytics.page();
-  }}();
-</script>
+// Segment Install
+// <script>
+//   !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+//   analytics.load("YwftWfBNKY9u0JVe3N1yG9yux5RdCggT");
+//   analytics.page();
+//   }}();
+// </script>
 
-<script>
-analytics.identify('f4ca124298', {
-  name: 'Michael Bolton',
-  email: 'mbolton@initech.com'
-});
-</script>
+// <script>
+// analytics.identify('f4ca124298', {
+//   name: 'Michael Bolton',
+//   email: 'mbolton@initech.com'
+// });
+// </script>
   
 //  <!-- Other Scripts-->
 //  <!-- Start of Async Drift Code -->
